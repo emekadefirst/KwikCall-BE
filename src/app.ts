ca65c1c8ab2d3d52ec07c6c496427e50807a84a8
emekadefirst <victorchibuogwu33@gmail.com>
2026-03-19 18:38:49 +0100

    change prod start command to npm

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ const trustedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
   'http://127.0.0.1:5500',
-  'https://lucky-people.outray.app'
+  'https://kwikcallui.pxxl.click'
 ];
 
 interface CorsOptions {
